eece9130cd7cd05f2cf4379c80ce91eb8204918a
meil <ics.sora.meil@gmail.com>
2017-01-25 15:53:14 +0900

    fix auto loading

---
+++ docs/Asset/Js/archive.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    $('#scroll').infinitescroll({
+        loading: {
+            finishedMsg: "",
+            msgText: "<em>読み込み中...</em>",
+            selector: null,
+            speed: 'fast',
+        },
+        navSelector: ".navigation",
+        nextSelector: ".navigation a",
+        itemSelector: ".scroll_content"
+    });
+});
